ebaafca24248651374aab053595d1b8578e9fdc5
Dan Visel <dbvisel@gmail.com>
2015-11-12 11:26:54 +0700

    mostly responsive version

--- app/js/crossdante.js
+++ app/js/crossdante.js
@@ -8,7 +8,10 @@ var canto = 0;
 var cantos = 4;
 var percentage = 0;
 var initialtextwidth = 0;
+
 var lineheight = 24;
+var lenswidth = 768; // assuming ipad in portrait
+var lensheight = 984; // assuming ipad in portrait
 
 
 $.fn.textWidth = function(text) {
@@ -33,29 +36,48 @@ function fixpadding(tofix) {
 				maxwidth = $(this).textWidth() + 18; // a little padding is necessary (~font size?)
 			}
 		});
-		$(tofix).css("padding","0 "+(initialtextwidth - maxwidth)/2+"px");
+		$(tofix).css({"padding-left":(initialtextwidth - maxwidth)/2+"px","padding-right":(initialtextwidth - maxwidth)/2+"px"});
 	} else {
 // this is prose, standardized padding
-		$(tofix).css("padding","0 80px");
+		var desiredwidth = lineheight * 16;
+		console.log(lenswidth + " "+desiredwidth);
+		var padding = (lenswidth - desiredwidth)/2;
+		console.log(padding);
+		if((desiredwidth + 20) > lenswidth) {
+			$(tofix).css({"padding-left":"10px","padding-right":"10px"});
+		} else {
+			$(tofix).css({"padding-left":padding+"px","padding-right":padding + "px"});
+		}
 	}
 }
 
-function recalclenssize() {
-	lensheight = document.window.height() - $("nav").height();
-	lenswidth = document.window.width();
-	if(lenswidth > lensheight) {
-		$("#lens").addClass("landscape").removeClass("portrait");
-	} else {
-		$("#lens").addClass("portrait").removeClass("landscape");
-	}
-	$("#navbar").css("width",lenswidth);
-	$("#lens").css({"width":lenswidth,"height":lensheight});
+function setpage(pageid) {
+	$(".page").removeClass("on");
+	$(".page#"+pageid).addClass("on");
+}
 
+function resize() {
+	var windowwidth = window.innerWidth;
+	var windowheight = window.innerHeight;
+	lenswidth = windowwidth;
+	lensheight = windowheight - $("nav").height();
+
+	if(lenswidth > lensheight) {
+		$(".page").addClass("landscape").removeClass("portrait");
+	} else {
+		$(".page").addClass("portrait").removeClass("landscape");
+	}
+	$("#main").css({"width":lenswidth+"px","height":windowheight+"px"});
+	$("#content").css({"width":lenswidth+"px","height":lensheight+"px"});
+
+	initialtextwidth = parseInt($("#text").css("width"),0);
+	setlens(translation,canto);
 }
 
 function rounded(h) {
 	return lineheight * Math.floor(h / lineheight);
 }
+
 
 function setlens(newtrans, newcanto) {
 //	var currentpercentage = ($("#text").scrollTop())/($("#text")[0].scrollHeight);
@@ -86,20 +108,18 @@ function setlens(newtrans, newcanto) {
 		$("#navtitle").html("&nbsp;");
 	}
 	fixpadding("#text",initialtextwidth);
-// this method still isn't great! it tries to round to current lineheight to avoid cutting off lines
 
-//	var scrollto = rounded((currentpercentage * ($("#text")[0].scrollHeight)));
+// this method still isn't great! it tries to round to current lineheight 
+// to avoid cutting off lines
+//var scrollto = rounded((currentpercentage * ($("#text")[0].scrollHeight)));
 	var scrollto = rounded((currentpercentage * ($("#text").prop('scrollHeight'))));
-
-
-
 	$("#text").scrollTop(scrollto);
 }
 
 
 $("document").ready(function() {
-	initialtextwidth = parseInt($("#text").css("width"),0);
-	setlens(0,0);
+	setpage("lens");
+	resize();
 	$("#navprev").click(function() {
 		setlens(translation-1,canto);
 	});
@@ -133,5 +153,8 @@ $("document").ready(function() {
 	}).keyup(function(e) {
 		e.preventDefault();
 		$(".button").removeClass("on");
+	});
+	$(window).resize(function() {
+		resize();
 	});
 });
