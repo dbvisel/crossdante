bba758e7ba55d03ab1e5405d3aa0806b03910634
Dan Visel <dbvisel@gmail.com>
2016-12-08 16:10:24 +0700

    single android release apk for inferno/purgatorio/paradiso

--- apps/crossdantepurgatorio/platforms/android/platform_www/cordova_plugins.js
+++ apps/crossdantepurgatorio/platforms/android/platform_www/cordova_plugins.js
@@ -45,12 +45,12 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-crosswalk-webview": "2.2.0",
     "cordova-plugin-device": "1.1.3",
     "cordova-plugin-inappbrowser": "1.5.0",
     "cordova-plugin-screen-orientation": "1.4.2",
     "cordova-plugin-splashscreen": "4.0.0",
-    "cordova-plugin-crosswalk-webview": "2.2.0"
+    "cordova-plugin-whitelist": "1.3.0"
 };
 // BOTTOM OF METADATA
 });
